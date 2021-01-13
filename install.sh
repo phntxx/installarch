b45feb9f2dee1ed34d90d708181bfca82f934ba8
phntxx <meissner.bastian@gmail.com>
2021-01-13 15:20:07 +0100

    Added scripts

---
+++ install.sh
@@ -0,0 +1,123 @@
+#! /bin/bash
+
+#
+# install.sh
+# phntxx/deployarch
+#
+
+function checkInternet {
+  echo "Checking internet connection by pinging archlinux.org..."
+  if ping -q -c 1 -W 1 archlinux.org > /dev/null; then
+    echo "Internet connection check successful, continuing..."
+  else
+    echo "Internet connection check failed, exiting."
+    exit
+  fi
+}
+
+function updateClock {
+  echo "Enabling NTP..."
+  timedatectl set-ntp true
+}
+
+function diskSetup {
+
+  validDiskInput=0
+  while [ $validDiskInput -ne "1" ]; do
+    read -p "What disk do you want to install Arch Linux on (e.g. /dev/sda)? " disk
+    if [ -b $disk ]; then
+      echo "Disk $disk is available, continuing..."
+      validDiskInput=1
+    else
+      echo "Disk $disk is not available, retrying."
+    fi
+  done
+
+  validSwapInput=0
+  while [ $validSwapInput -ne "1" ]; do
+    read -p "What disk do you want to install Arch Linux on (e.g. 5G)? " swap
+    if [ $swap =~ ^[1-9][0-9]*[MG]$ ]; then
+        echo "Swap partition will be $swap in size, continuing..."
+        validSwapInput=1
+    else
+      echo "Invalid Input, retrying."
+    fi
+  done
+
+  echo "This script will now partition and format your disk as follows:"
+  echo "Partition 1: 300M, mounted at /mnt/boot, used for GRUB"
+  echo "Partition 2: XXXM, used for swap"
+  echo "Partition 3: remaining disk space, used for Arch Linux"
+  echo "During this process, all of the data that is currently on the disk will be lost."
+
+  validConfirmInput=0
+  while [ $validInput -ne "1" ]; do
+
+    read -p "Do you want to continue? [Y/N] " formatConfirm
+
+    if [ $formatConfirm == "y" ] || [ $formatConfirm == "Y" ]; then
+      validConfirmInput=1
+      echo "Agreed to partitioning and formatting, continuing..."
+    elif [ $formatConfirm == "n" ] || [ $formatConfirm == "N" ]; then
+      validConfirmInput=1
+      echo "User did not agree to partitioning and formatting, exiting."
+      exit
+    else
+      echo "Invalid Input, retrying."
+    fi
+  done
+
+
+  echo "Partitioning disk..."
+  # https://superuser.com/a/332322
+  (
+    o
+    n
+    p
+    1
+
+    +300M
+    n
+    p
+    2
+
+    +$disk
+    n
+    p
+    3
+
+
+    a
+    1
+    p
+    w
+    q
+  ) | fdisk
+
+  echo "Formatting disk, enabling and mounting partitions..."
+
+  mkfs.fat -F 32 ${disk}1
+  mkfs.ext4 ${disk}3
+
+  mkswap ${disk}2
+  swapon ${disk}2
+
+  mount ${disk}3 /mnt
+  mkdir /mnt/boot
+  mount ${disk}1 /mnt/boot
+}
+
+function installArch {
+  echo "Installing Arch Linux..."
+  pacstrap /mnt base base-devel linux linux firmware
+}
+
+if [[ $EUID -eq 0 ]]; then
+   checkInternet
+   updateClock
+   diskSetup
+   installArch
+else
+  echo "This script must be run as root."
+  exit
+fi
---
+++ post-install.sh
@@ -0,0 +1,100 @@
+#! /bin/bash
+
+#
+# post-install.sh
+# phntxx/deployarch
+#
+
+#!/bin/bash
+
+function installPackages {
+  echo "Installing efibootmgr, dosfstools, gptfdisk, grub, networkmanager and vim..."
+  pacman -Sy efibootmgr dosfstools gptfdisk grub networkmanager vim
+}
+
+function configureVM {
+  validVMInput=0
+  while [ $validInput -ne "1" ]; do
+
+    read -p "Is this Arch Linux install running inside of a VMware virtual machine? [Y/N]" vmConfirm
+
+    if [ $vmConfirm == "y" ] || [ $vmConfirm == "Y" ]; then
+      validVMInput=1
+      installVMTools
+    elif [ $vmConfirm == "n" ] || [ $vmConfirm == "N" ]; then
+      validVMInput=1
+    else
+      echo "Invalid Input. Please try again."
+    fi
+  done
+
+  validGFXInput=0
+  while [ $validGFXInput -ne "1" ]; do
+
+    read -p "Do you require VMware graphics patches? [Y/N]" gfxConfirm
+
+    if [ $gfxConfirm == "y" ] || [ $gfxConfirm == "Y" ]; then
+      validGFXInput=1
+      installVMGFX
+    elif [ $gfxConfirm == "n" ] || [ $gfxConfirm == "N" ]; then
+      validGFXInput=1
+    else
+      echo "Invalid Input. Please try again."
+    fi
+  done
+
+}
+
+function installVMTools {
+  echo "Installing open-vm-tools"
+  pacman -S open-vm-tools
+  systemctl enable --now vmtoolsd.service
+  systemctl enable --now vmware-vmblock-fuse.service
+}
+
+function installVMGFX {
+  echo "Installing VMware graphics drivers"
+  pacman -S xf86-input-vmmouse xf86-video-vmware mesa gtk2 gtkmm
+  echo needs_root_rights=yes >> /etc/X11/Xwrapper.config
+}
+
+function configureSystem {
+
+  read -e "What is your keyboard layout?" keyboardLayout
+
+  echo "Setting $keyboardLayout as the layout in /etc/vconsole.conf..."
+
+  touch /etc/vconsole.conf
+  echo "KEYMAP=$keyboardLayout" >> /etc/vconsole.conf
+
+  read -e "What is the hostname of this machine?" hostname
+
+  echo "Setting $hostname as the hostname of this machine in /etc/hostname..."
+
+  touch /etc/hostname
+  echo "$hostname" >> /etc/hostname
+
+  echo "Adding the appropriate entries in /etc/hosts..."
+
+  echo "127.0.0.1 localhost" >> /etc/hosts
+  echo "::1 localhost" >> /etc/hosts
+  echo "127.0.0.1 $hostname.localdomain $hostname" >> /etc/hosts
+}
+
+function installBootloader {
+  echo "Installing GRUB..."
+  grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch_grub --recheck --debug
+  mkdir /boot/grub/locale
+  cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
+  grub-mkconfig -o /boot/grub/grub.cfg
+}
+
+if [[ $EUID -eq 0 ]]; then
+   installPackages
+   configureVM
+   configureSystem
+   installBootloader
+else
+  echo "This script must be run as root."
+  exit
+fi
